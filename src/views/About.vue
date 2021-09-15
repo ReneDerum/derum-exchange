e515d2d37b7c294a5915a243355a00d75d0cf0d2
renederum <renederum@gmail.com>
2021-09-14 20:22:49 -0400

    Project kick iff

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl text-center">Derum Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas mas importante
+      a traves de un API.
+      <br />
+      Este proyecto es utilizado en el curso de Vue.js Basico de Platzi.
+    </p>
+    <router-link
+      to="/"
+      class="text-green-500 text-center hover:underline uppercase"
+      >Volver a Home</router-link
+    >
+  </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl text-center">Error</h1>
+    <p class="text-gray-600 text-xl text-center">
+      La ruta especificada no exite, volver a home.
+      <br />
+      Este proyecto es utilizado en el curso de Vue.js Basico de Platzi.
+    </p>
+    <router-link
+      to="/"
+      class="text-green-500 text-center hover:underline uppercase"
+      >Volver a Home</router-link
+    >
+  </div>
+</template>
